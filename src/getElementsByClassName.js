b31527e573a1a2e620d05e9bc155073c3d7486b1
tylertruong <tylertruong@gmail.com>
2017-08-21 15:09:23 -0700

    completed getElementsByClassNameSpec.js and fixed test cases for stringifyJSON and getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -3,8 +3,29 @@
 //   return document.getElementsByClassName(className);
 // };
 
+// You should use: document.body, element.childNodes, and element.classList
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var element = arguments[1] || document.body;
+  var result = [];
+
+// if the element has the className inside it
+  if(element.classList && element.classList.contains(className)) {
+    result = result.concat(element); 
+  }
+// if the element has children, 
+  if(element.childNodes){
+  //if it does, run function again
+    for (var i = 0; i < element.childNodes.length; i++) {
+      result = result.concat(getElementsByClassName(className, element.childNodes[i])); 
+    }
+    
+  }
+
+ return result;
 };
+
+
+
+//[]
